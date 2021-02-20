b3b940c3f74a64fd76e34e56266c38b42f744c1d
Jarvo <34064987+JarvoDiscord@users.noreply.github.com>
2021-02-20 21:29:36 +0100

    Update say.js

--- small/commands/say.js
+++ small/commands/say.js
@@ -6,7 +6,7 @@ module.exports = {
 
 	async execute(client, message, args) {
 		if(message.author.bot) return;
-		console.log('uhm')
+
 		if(!args.length) return message.reply('Ja... Hier kan ik niks mee. Wat moet ik nou zeggen dan?')
 		message.channel.send(args);
 	},
